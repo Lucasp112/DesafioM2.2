ba7a1edd90ee2f2e08dde3008eaca88c667a663f
Lucasp122 <lucasbernardo1895@gmail.com>
2023-01-30 10:49:40 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,30 @@
+function mostrarNome(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+mostrarNome("Lucas")
+
+function novaFuncao(nome, idade, musica){
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical é ${musica}`)
+
+}
+novaFuncao('lucas', 17, 'Trap')
+
+function filmeFuncao(filme, musicapref){
+    console.log(`Meu filme preferido é ${filme} e minha música preferida é ${musicapref}`)
+
+}
+filmeFuncao('Bastardos inglórios', 'Algumas coisas')
+
+function funcao(numero){
+    return numero*2
+
+}
+console.log(funcao(16))
+
+let diaEnsolarado = true
+
+function novaFuncao(dia){
+console.log(dia)
+}
+
+novaFuncao(diaEnsolarado)
